0e712a8440b0ac422b90dd3026ac94e33331785a
Max Eriksson <maxerikss@gmail.com>
2024-09-21 22:37:29 +0200

    Changed to camelCase

--- rydbergHelper.py
+++ rydbergHelper.py
@@ -15,53 +15,53 @@ import pandas as pd
 client_ID = "02e805b7-77ee-11ef-b441-6d6e84e75559"
 API_key = "eyJraWQiOiIwIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJpWmV0dGxlIiwiYXVkIjoiQVBJIiwiZXhwIjoyNjczNjEyNzM2LCJzdWIiOiJlNGJiZDM5MC1jYmEwLTExZTctYTlhYS1jOWQ4ZmYzZDk3MWQiLCJpYXQiOjE3MjY5MDQ5NjAsImNsaWVudF9pZCI6IjAyZTgwNWI3LTc3ZWUtMTFlZi1iNDQxLTZkNmU4NGU3NTU1OSIsInR5cGUiOiJ1c2VyLWFzc2VydGlvbiIsInVzZXIiOnsidXNlclR5cGUiOiJVU0VSIiwidXVpZCI6ImU0YmJkMzkwLWNiYTAtMTFlNy1hOWFhLWM5ZDhmZjNkOTcxZCIsIm9yZ1V1aWQiOiJlNGJhNzQwMC1jYmEwLTExZTctOGJmYi1lMjA3NzRhZWEwZjUiLCJ1c2VyUm9sZSI6Ik9XTkVSIn0sInNjb3BlIjpbIlJFQUQ6UFJPRFVDVCJdfQ.ITx5w9cuNr7CeCrR4mZGD5XTITWy201VQoiiTUQ_q5rR9wDkBTnoP1mWqP76TGgbgwrRsvFXAyyues2FMhbhAuuZF3pFJoKOt5fwWB2b1xJ8Z2iPSOq33WnONDiwNtvLCwK152FxhDYWL4SFo2bjtG_bOyMrN4d4g5dJlyazR9WSohk1kYGkgVavpe96RaIzW3LPM4MQhFfz7oHqAfUkOiMKYx6GB7jQikaFIvgZHm4G3NmY_AsBGLM48ZNz2PoJa6j9mSYOgwrqKfUvOHpyRqHHIS1uoSEFfCq6KdKAVcpYfFh7qKMkXkobFtd7Wnfaa5BMHgx17dA8mG4nTtKiTg"
 
-post_url = "https://oauth.zettle.com/token"
+postUrl = "https://oauth.zettle.com/token"
 
-post_headers = {
+postHeaders = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
-post_data = {
+postData = {
     "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
     "client_id": f"{client_ID}",
     "assertion": f"{API_key}"     
 }
 
-access = requests.post(post_url, headers=post_headers, data=post_data)
+access = requests.post(postUrl, headers=postHeaders, data=postData)
 
-access_data = access.json()
-bearer_token = access_data.get("access_token")
+accessData = access.json()
+bearerToken = accessData.get("access_token")
 
 
-library_url = "https://products.izettle.com/organizations/self/library"
-stock_url = "https://inventory.izettle.com/v3/stock"
-get_headers = {
-    "Authorization": f"Bearer {bearer_token}",  
+libraryUrl = "https://products.izettle.com/organizations/self/library"
+stockUrl = "https://inventory.izettle.com/v3/stock"
+getHeaders = {
+    "Authorization": f"Bearer {bearerToken}",  
 }
 
-library_response = requests.get(library_url, headers=get_headers)
+libraryResponse = requests.get(libraryUrl, headers=getHeaders)
 
-all_pages = False
-stock_response = []
-stock_response.append(requests.get(stock_url, headers=get_headers))
-while not all_pages:
-    link_header = stock_response[-1].headers.get("Link")
-    if link_header == None:
-        all_pages = True
+allPages = False
+stockResponse = []
+stockResponse.append(requests.get(stockUrl, headers=getHeaders))
+while not allPages:
+    linkHeader = stockResponse[-1].headers.get("Link")
+    if linkHeader == None:
+        allPages = True
     else:
-        links = link_header.split(", ")
-        next_url = None
+        links = linkHeader.split(", ")
+        nextUrl = None
         for link in links:
             url, rel = link.split("; ")
             if 'rel="next"' in rel:
-                next_url = url.strip("<>")
-        stock_response.append(requests.get(next_url, headers=get_headers))
+                nextUrl = url.strip("<>")
+        stockResponse.append(requests.get(nextUrl, headers=getHeaders))
 
 # Data of the products and the inventory balance
-products_data = library_response.json().get("products")
-stock_data = []
-for i in stock_response:
-    stock_data.extend(i.json())
+productsData = libraryResponse.json().get("products")
+stockData = []
+for i in stockResponse:
+    stockData.extend(i.json())
 
 # +---------------------------------------------+
 # | ######  ##          ###     ######   ###### |
@@ -88,11 +88,11 @@ class Product:
         print(f"Price: {self.price}")
         print(f"Balance: {self.balance}")
 
-list_products = []
+listProducts = []
 
 
-for i in products_data:
-    list_products.append(Product(
+for i in productsData:
+    listProducts.append(Product(
         i.get("uuid"),
         i.get("name"),
         i.get("category").get("name"),
@@ -101,14 +101,14 @@ for i in products_data:
         )
     )
 
-for i in stock_data:
+for i in stockData:
     uuid = i.get("productUuid")
     balance = i.get("balance")
-    for j in list_products:
+    for j in listProducts:
         if j.uuid == uuid and j.category in ["Beer", "Cider", "Wine"]:
             j.balance = balance
         
 
-for i in list_products:
+for i in listProducts:
     print("--------------------------")
     i.print()
